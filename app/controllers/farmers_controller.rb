a4e11de540d59b8257ec87893aabc4b100d724a2
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-09-19 15:11:57 +0200

    Carousel home page

---
+++ app/controllers/farmers_controller.rb
@@ -0,0 +1,38 @@
+class FarmersController < ApplicationController
+  def index
+    @farmers = Farmer.all
+  end
+
+  def show
+    @farmers = Farmer.find(params[:id])
+  end
+
+  def new
+    @farmer = Farmer.new
+  end
+
+  def create
+    @farmer = Farmer.new(farmer_params)
+    @farmer.save
+  end
+
+  def edit
+  end
+
+  def update
+    @farmer = Farmer.find(params[:id])
+    @farmer.update(farmer_params)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def farmer_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:farmer).permit(:first_name, :last_name, :farm_name, :origin, :region)
+  end
+
+end
