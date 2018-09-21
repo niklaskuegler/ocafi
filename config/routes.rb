de2763cffb29ffe8b3b51bd210843557fd9a9cd8
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-09-21 15:09:01 +0200

    People can now sign up for the newsletter

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :farmers
+
+  resources :newsletters, only: [:create]
+
+  #post 'newsletters', to: 'newsletters#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
