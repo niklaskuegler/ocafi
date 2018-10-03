2bc46941be932f70882b7e6fd76876589b2949dd
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-10-03 14:29:27 +0200

    Route is established, leading to our-coffees

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about_us, :mission_vision, :general_terms_condition_supply, :general_terms_condition_purchase, :privacy_policy]
+  skip_before_action :authenticate_user!, only: [:home, :about_us, :mission_vision, :general_terms_condition_supply, :general_terms_condition_purchase, :privacy_policy, :farmers]
 
   def home
   end
