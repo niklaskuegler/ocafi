9e8314f0814783d9ff4313c011bd6d0925b496fa
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-10-23 12:54:13 +0200

    short and long description for farmers added to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_170558) do
+ActiveRecord::Schema.define(version: 2018_10_23_105321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2018_10_22_170558) do
     t.string "farm_landscape8"
     t.string "farm_landscape9"
     t.string "farm_landscape10"
+    t.string "short_description"
+    t.string "long_description"
   end
 
   create_table "newsletters", force: :cascade do |t|
