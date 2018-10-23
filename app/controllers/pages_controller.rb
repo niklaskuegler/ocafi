1535928e9c1efbfbb79da7ccc481946a441ccc02
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-10-23 18:24:29 +0200

    small small details like headings fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :show, :index, :farmer, :farmers, :our_coffee, :mission_vision, :general_terms_condition_supply, :general_terms_condition_purchase, :privacy_policy]
+  skip_before_action :authenticate_user!, only: [:home, :about_us, :show, :index, :farmer, :farmers, :our_coffee, :mission_vision, :general_terms_condition_supply, :general_terms_condition_purchase, :privacy_policy]
 
   def home
   end
