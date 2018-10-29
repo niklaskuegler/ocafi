2c6c9fead092a46f453e521aae7592ea8a5cbb53
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-10-29 18:47:21 +0100

    initial admin model deleted

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 2018_10_23_140032) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
