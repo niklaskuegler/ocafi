12ebdc4a339527e3e23a56338c621b9a5acad8df
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-10-29 18:41:47 +0100

    application record cleaned

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  ActiveRecord::Base.connection.tables.each do |t|
-    ActiveRecord::Base.connection.reset_pk_sequence!(t)
-  end
-
-
 end
