3e65a3391a0eabed0c60cec79a6c07cd0fd0c002
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-11-05 19:08:52 +0100

    popup working smooth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_121440) do
+ActiveRecord::Schema.define(version: 2018_11_05_180814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_121440) do
     t.boolean "active"
     t.string "name"
     t.float "price2"
+    t.string "cup_description"
     t.index ["farmer_id"], name: "index_coffees_on_farmer_id"
   end
 
