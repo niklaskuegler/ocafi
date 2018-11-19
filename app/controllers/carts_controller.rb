c5b7d8c92beb265a50abc9612b4b88563b47c462
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-11-19 18:58:00 +0100

    carts controller updated with mailer

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,10 +10,32 @@ class CartsController < ApplicationController
 
   def update
     @order = current_order
-    @order.update(order_params)
+    if @order.update(order_params)
 
-    #add confirmation link below this
-    redirect_to cart_path
+      #Update trigger for mail - check for validations
+      company_name = params[:order][:company_name]
+      customer_first_name = params[:order][:customer_first_name]
+      customer_last_name = params[:order][:customer_last_name]
+      customer_email = params[:order][:customer_email]
+      phone_number = params[:order][:phone_number]
+      customer_billing_address = params[:order][:customer_billing_address]
+      customer_billing_postcode = params[:order][:customer_billing_postcode]
+      customer_billing_city = params[:order][:customer_billing_city]
+      customer_billing_country = params[:order][:customer_billing_country]
+      vat_number = params[:order][:vat_number]
+      special_request = params[:order][:special_request]
+
+      OrderMailer.order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request).deliver
+
+      #flash does not work yet
+      flash[:success] = "Order placed"
+
+      #add confirmation link below this
+      redirect_to root_path
+
+    else
+      redirect_to cart_path
+    end
   end
 
   private
