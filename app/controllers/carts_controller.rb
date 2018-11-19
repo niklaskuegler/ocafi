6bfcf7001d5d652b0096a57ee24df49851882ba1
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-11-19 18:23:41 +0100

    order mailer added, and order controller removed - was double

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,10 +11,13 @@ class CartsController < ApplicationController
   def update
     @order = current_order
     @order.update(order_params)
+
+    #add confirmation link below this
     redirect_to cart_path
   end
 
   private
+
   def order_params
     params.require(:order).permit(:company_name, :customer_first_name, :customer_last_name, :customer_billing_address, :customer_billing_postcode, :customer_billing_city, :customer_billing_country, :customer_billing_state, :vat_number, :special_request)
   end
