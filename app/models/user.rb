899cb55cbad36cd79c626682ce4586a6baf1676a
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-11-19 18:02:49 +0100

    deleted user mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,9 @@
 class User < ApplicationRecord
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
-  after_create :send_welcome_email
 
   def set_default_role
     self.role ||= :user
-  end
-
-  private
-
-  def send_welcome_email
-    UserMailer.welcome(self).deliver_now
   end
 
   # Include default devise modules. Others available are:
