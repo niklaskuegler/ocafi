8f62a8bd86ddb58e05c329a3165311fac56a6757
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-11-20 14:07:47 +0100

    and again, not giving up

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,7 +9,8 @@ class CartsController < ApplicationController
     @order = current_order
     if @order.update(order_params)
 
-      @order = current_order
+      order = current_order
+      order_items = current_order.order_items
 
       #Update trigger for mail - check for validations
       company_name = params[:order][:company_name]
@@ -24,7 +25,7 @@ class CartsController < ApplicationController
       vat_number = params[:order][:vat_number]
       special_request = params[:order][:special_request]
 
-      OrderMailer.order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request, @order).deliver
+      OrderMailer.order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request, order, order_items).deliver
 
       #flash does not work yet
       flash[:success] = "Order placed"
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,10 @@
 class OrderMailer < ApplicationMailer
   default to: 'info@ocafi.de'
 
-  def order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request, order)
+
+  def order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request, order, order_items)
+    @order = order
+    @order_items = order_items
 
     @company_name = company_name
     @customer_email = customer_email
