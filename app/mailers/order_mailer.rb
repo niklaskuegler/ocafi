33b57d90be983337d64aaeb53d24c03aaee7d7e4
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-11-20 09:51:51 +0100

    logic mailer controller

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < ActionMailer::Base
   default to: 'info@ocafi.de'
 
-  def order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request, current_order_items)
+  def order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request)
     @company_name = company_name
     @customer_email = customer_email
     @phone_number = phone_number
@@ -13,7 +13,6 @@ class OrderMailer < ActionMailer::Base
     @customer_billing_country = customer_billing_country
     @vat_number = vat_number
     @special_request = special_request
-    @current_order_items = current_order_items
 
     mail(from: customer_email, subject: 'Customer Order Details')
   end
