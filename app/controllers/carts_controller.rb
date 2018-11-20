41ac40769fd0928a8c9360454da0adaab0726a20
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-11-20 09:25:11 +0100

    quantity should be included in mail

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,9 +7,12 @@ class CartsController < ApplicationController
 
   def update
     @order = current_order
+    @order_items = current_order.order_items
+
     if @order.update(order_params)
 
       #Update trigger for mail - check for validations
+
       company_name = params[:order][:company_name]
       customer_first_name = params[:order][:customer_first_name]
       customer_last_name = params[:order][:customer_last_name]
@@ -22,7 +25,9 @@ class CartsController < ApplicationController
       vat_number = params[:order][:vat_number]
       special_request = params[:order][:special_request]
 
-      OrderMailer.order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request).deliver
+      quantities = @order_items
+
+      OrderMailer.order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request, quantities).deliver
 
       #flash does not work yet
       flash[:success] = "Order placed"
@@ -39,6 +44,10 @@ class CartsController < ApplicationController
 
   def order_params
     params.require(:order).permit(:company_name, :customer_first_name, :customer_last_name, :customer_billing_address, :customer_billing_postcode, :customer_billing_city, :customer_billing_country, :customer_billing_state, :vat_number, :special_request)
+  end
+
+  def order_items_params
+    params.require(:order_items).permit(:quantity, :sample, :coffee_id)
   end
 
 end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,10 @@
 class OrderMailer < ActionMailer::Base
   default to: 'info@ocafi.de'
 
-  def order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request)
+  def order_email(company_name, customer_first_name, customer_last_name, customer_email, phone_number, customer_billing_address, customer_billing_postcode, customer_billing_city, customer_billing_country, vat_number, special_request, quantities)
     @company_name = company_name
     @customer_email = customer_email
+    @phone_number = phone_number
     @customer_first_name = customer_first_name
     @customer_last_name = customer_last_name
     @customer_billing_address = customer_billing_address
@@ -12,6 +13,7 @@ class OrderMailer < ActionMailer::Base
     @customer_billing_country = customer_billing_country
     @vat_number = vat_number
     @special_request = special_request
+    @quantities = quantities
 
     mail(from: customer_email, subject: 'Customer Order Details')
   end
